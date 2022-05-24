abc562061bd0a55f20b5fd4257d56c53b2258e15
Guinle Loic <101970621+Guinlel@users.noreply.github.com>
2022-05-24 15:52:00 +0200

    Offline Mode

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,11 @@
 const CACHE_NAME = "RADSP"
 const Cache_Files= [
-    '/src/App.js',
-    '/src/components/Home.js'
+    'static/js/main.chunk.js',
+    'static/js/0.chunk.js',
+    'static/js/bundle.js',
+    'leaflet@1.8.0/dist/leaflet.js',
+    'manifest.json',
+    'favicon.ico'
 ]
 
 // Installing SW
@@ -11,13 +15,21 @@ self.addEventListener("install",(event)=>{
 });
 
 // SW Respond
+self.addEventListener("fetch", event => {
+    console.log(`Request of ${event.request.url}`);
+  
+    // comportement par défaut: requête le réseau
+    event.respondWith(fetch(event.request));
+  });
+
 self.addEventListener('fetch',(event)=>{
-    event.respondWith(caches.match(event.request)
-        .then(cached => cached || fetch(event.request))
+    event.respondWith(
+        caches.match(event.request).then((cached)=> cached || fetch(event.request))
     )
-})
+});
 
 // SW activate
 self.addEventListener("activate",(event)=>{
     console.log("Services activating.");
 })
+
