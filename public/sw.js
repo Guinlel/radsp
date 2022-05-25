397eb656f5b9a02c21881b00e65ffe4a4fc73ea4
Guinle Loic <101970621+Guinlel@users.noreply.github.com>
2022-05-25 09:38:09 +0200

    Working offline mode

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,14 @@
 const CACHE_NAME = "RADSP"
 const Cache_Files= [
+    '/',
     'static/js/main.chunk.js',
     'static/js/0.chunk.js',
     'static/js/bundle.js',
     'leaflet@1.8.0/dist/leaflet.js',
     'manifest.json',
-    'favicon.ico'
+    'favicon.ico',
+    'sockjs-node/info?t=1653402190477',
+    'logo192.png'
 ]
 
 // Installing SW
@@ -15,16 +18,24 @@ self.addEventListener("install",(event)=>{
 });
 
 // SW Respond
-self.addEventListener("fetch", event => {
-    console.log(`Request of ${event.request.url}`);
+// self.addEventListener("fetch", event => {
+//     console.log(`Request of ${event.request.url}`);
   
-    // comportement par défaut: requête le réseau
-    event.respondWith(fetch(event.request));
-  });
+//     // comportement par défaut: requête le réseau
+//     event.respondWith(fetch(event.request));
+//   });
 
-self.addEventListener('fetch',(event)=>{
+self.addEventListener("fetch",(event)=>{
     event.respondWith(
-        caches.match(event.request).then((cached)=> cached || fetch(event.request))
+        caches.match(event.request).
+        catch(()=>{ return fetch(event.request)
+        .then(()=>{
+            return cache.open(CACHE_NAME).then(()=>{
+                caches.put(event.request, response.clone());
+                return response;
+            })
+        })
+        })
     )
 });
 
