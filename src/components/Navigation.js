0b0943238b08a769adb3a59960f41cf2a4d07740
Guinle Loic <101970621+Guinlel@users.noreply.github.com>
2022-06-07 17:23:58 +0200

    Rajout de Radionucleides

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,6 @@ import {ReactComponent as CloseLogo} from '../img/close.svg'
 
 function Navigation() {
     const [isActive, setActive] = useState('false');
-    const nav = document.getElementById('navigation');
 
     useEffect(()=>{
         document.getElementById('close-img').addEventListener('click',(e)=>{
@@ -24,16 +23,16 @@ function Navigation() {
             <nav id='navigation' className={isActive ? "navigation" : "navigation-visible"}>
                 <CloseLogo id="close-img" className="close-img" />
                 <Link to="/" className="link">Accueil</Link>
-                <Link to="/radionucleide" className="link">Radionucleide</Link>
+                <Link to="/radionucleide" className="link">Radionucléides</Link>
                 <Link to="/conversion" className="link"> Conversion Ci vers Bq</Link>
                 <Link to="/calcul-dose" className="link">Calcul de dose selon la distance</Link>
                 <Link to="/calcul-perimetre" className="link">Calcul de distance selon la dose</Link>
                 <Link to="/activite-restante" className="link"> Activité restante </Link>
                 <Link to="/etiquette" className="link">Etiquettes de Transport</Link>
                 <Link to="/meteo" className="link">Meteo</Link>
+        
             </nav>
         </div>
-
     )
 
 }
